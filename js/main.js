7004719fd2cef104df25ee7c625a558fff5b964b
itsandromeda <itsandromeda@gmail.com>
2017-03-11 19:43:22 -0500

    Button functionality changes

--- js/main.js
+++ js/main.js
@@ -1,51 +1,64 @@
 /*jslint eqeq: true*/
-/*global window, document, alert, sum, sub, mult, div*/
-var operation,
-    result,
-    si = document.getElementById("si"),
-    no = document.getElementById("no"),
-    nombre,
-    num1,
-    num2;
-
-function oper() {
+/*global window, document, alert, prompt, confirm, number1, number2, name*/
+document.getElementById('buttons').addEventListener('click', function (e) {
     "use strict";
-    switch (operation) {
-    case 1:
-        var r = num1 + num2;
-        result.innerHTML = "<span class='results'> Hola " + nombre + ",<br>La suma entre " + num1 + " y " + num2 + " es " + r + "<span>";
-        break;
+    var target = e.target,
+        nombre = document.getElementById("name").value,
+        num1 = parseInt((document.getElementById("number1").value), 10),
+        num2 = parseInt((document.getElementById("number2").value), 10),
+        operation = parseFloat(document.getElementById("operation").value),
+        result = document.getElementById("answer"),
+        r,
+        newNumber,
+        op1,
+        newNum1;
 
-    case 2:
-        r = num1 - num2;
-        result.innerHTML = "<span class='results'>Hola " + nombre + ",<br>La resta entre " + num1 + " y " + num2 + " es " + r + "<span>";
-        break;
+    function oper() {
+        switch (operation) {
+        case 1:
+            r = num1 + num2;
+            break;
 
-    case 3:
-        r = num1 * num2;
-        result.innerHTML = "<span class='results'>Hola " + nombre + ",<br>La multiplicación entre " + num1 + " y " + num2 + " es " + r + "<span>";
-        break;
+        case 2:
+            r = num1 - num2;
+            break;
 
-    case 4:
-        r = num1 / num2;
-        result.innerHTML = "<span class='results'>Hola " + nombre + ",<br>La división entre " + num1 + " y " + num2 + " es " + r + "<span>";
-        break;
+        case 3:
+            r = num1 * num2;
+            break;
 
-    default:
-        result.innerHTML = "Si no ingresas un número no se puede hacer una operación :o";
+        case 4:
+            r = num1 / num2;
+            break;
+
+        default:
+            result.innerHTML = "Si no ingresas un número no se puede hacer una operación :o";
+        }
     }
-}
 
-no.addEventListener("click", function () {
-    "use strict";
-    nombre = document.getElementById("name").value;
-    num1 = parseInt((document.getElementById("number1").value), 10);
-    num2 = parseInt((document.getElementById("number2").value), 10);
-    operation = parseFloat(document.getElementById("operation").value);
-    result = document.getElementById("answer");
-    oper();
+    if (target.id === 'si') {
+        if (confirm("Por favor ingresa una nueva operación y un nuevo número.") === true) {
+            document.getElementById('number1').disabled = true;
+            document.getElementById("operation").selectedIndex = 0;
+            oper();
+            number1.value = r;
+            oper();
+        } else {
+            number1.value = "";
+            number2.value = "";
+            document.getElementById("operation").selectedIndex = 0;
+            oper();
+            result.innerHTML = "<span class='results'>" +
+                                    "Hola <strong>" + nombre + "</strong>,<br>" +
+                                    "El resultado de tu operación es " + r +
+                                "<span>";
+
+        }
+    } else if (target.id === 'no') {
+        oper();
+        result.innerHTML = "<span class='results'>" +
+                                "Hola <strong>" + nombre + "</strong>,<br>" +
+                                "El resultado de tu operación es " + r +
+                            "<span>";
+    }
 });
-/*
-si.addEventListener("click", function () {
-    "use strict";
-});*/
